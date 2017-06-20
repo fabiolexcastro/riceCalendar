8b86c2ea417a6063bdbdbdea772463b135e20a14
Fabio Lex Castro <fabioalexandercastro@gmail.com>
2017-06-20 10:55:02 -0500

    Add 32 Bit Float

--- riceCalendar/mosaicRaster.py
+++ riceCalendar/mosaicRaster.py
@@ -3,8 +3,9 @@
 
 import arcpy
 
-arcpy.env.workspace  = r'Z:\RiceCalendar\_raster\_climate\_delta\_tif\_prec'
-outputFolder = r'Z:\RiceCalendar\_raster\_climate\_mosaicDelta'
+arcpy.env.workspace  = r'W:\RiceCalendar\_raster\_climate\_delta2\_tif\_tmean'
+outputFolder = r'W:\RiceCalendar\_raster\_climate\_mosaicDelta'
 rasterList = arcpy.ListRasters('*','')
-arcpy.MosaicToNewRaster_management(rasterList, outputFolder + "prec" + ".tif", "", "32_BIT_SIGNED", "", "1", "LAST", "FIRST")	
+print rasterList
+arcpy.MosaicToNewRaster_management(rasterList, outputFolder, "tmean2.tif", "", "32_BIT_FLOAT", "", "1", "LAST", "FIRST")	
 print "Finish"
